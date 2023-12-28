a252bab1c1994c9e5e8bf72027f14e7f13861030
Justin Braben <jbraben@dmsiworks.com>
2023-12-27 17:10:17 -0700

    Update argument parser

--- src/main.zig
+++ src/main.zig
@@ -3,47 +3,22 @@ const c = @cImport({
 });
 const std = @import("std");
 const Args = @import("gb/Args.zig").Args;
-const errors = @import("gb/Errors.zig");
-const assert = @import("std").debug.assert;
-const Registers = @import("gb/Cpu.zig").Registers;
-const Emu = @import("gb/Emu.zig").Emu;
-const RomTypes = @import("gb/configs/RomTypes.zig").RomTypes;
-const ArrayList = std.ArrayList;
+
 const print = std.debug.print;
 
 pub fn main() !void {
-    std.debug.print("\n", .{});
+    print("\n", .{});
 
-    const args = Args.parse_args() catch |err| {
-        switch (err) {
-            errors.ControlledExit.Help => {
-                std.os.exit(0);
-            },
-            else => {
-                std.log.err("Unknown error {any}\n", .{err});
-                std.os.exit(5);
-            },
-        }
-    };
+    var gpa_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.testing.expect(gpa_allocator.deinit() != .leak) catch @panic("memory leak");
+    const gpa = gpa_allocator.allocator();
 
-    std.debug.print("Arguments : {}\n", .{args});
+    var parse_args = try Args.init(gpa);
+    defer parse_args.deinit();
 
-    // var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    // defer arena.deinit();
+    print("Args : {any}\n", .{parse_args});
 
-    // const alloc = arena.allocator();
-
-    // var emu = Emu.new(alloc, "../roms/Legend of Zelda, The - Link's Awakening (G) [!].gb");
-
-    // std.debug.print("Created new Emu : '{s}' , of type : '{}'\n", .{ emu.romPath, @TypeOf(emu.romPath) });
-
-    // std.debug.print("emu_get_context returns this type: {}\n", .{@TypeOf(emu.emu_get_context())});
-
-    // // for (RomTypes) |RomType| {
-    // //     std.debug.print("Rom Type : {s}\n", .{RomType});
-    // // }
-
-    // const returnVal = try emu.emu_run();
-
-    // std.debug.print("Returned code: {}\n", .{returnVal});
+    // while (parse_args.args_allocated.next()) |arg| {
+    //     print("arg : {s}\n", .{arg});
+    // }
 }
